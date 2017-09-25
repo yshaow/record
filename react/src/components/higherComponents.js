e4a511a5b0d5c86f17f92defac814acc29c5453b
yangshao <yshaow@126.com>
2017-09-25 08:09:12 +0800

    update react

--- react/src/components/higherComponents.js
+++ react/src/components/higherComponents.js
@@ -86,6 +86,94 @@ class BlogPost extends React.Component{
 const CommentListWidthSubscription = withSubscription(CommentList,DataSource => DataSource.getComments());
 const BlogPostWidthSubscription = withSubscription(BlogPost,(DataSource,props) => DataSource.getBlogPost(props.id));
 
+/**
+ *
+ * @param WrappedComponent 被包含的组件
+ * @param selectData 函数--根据给定的DataSource和当前props取回我们需要的数据
+ */
+function widthSubscription(WrappedComponent,selectData){
+
+    return HigherComponents;
+}
+
+/**
+ * 当 CommentListWidthSubscription与BlogPostWidthSubscription被渲染的时候，CommentList和BlogPost将会
+ * 被传递data属性，其中包含从DataSource取回的最新数据
+ */
+class HigherComponents extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {
+            data:selectData(DataSource,props)
+        }
+    }
+
+    componentDidMount(){
+        DataSource.addChangeListener(this.handleChange);
+    }
+
+    componentWillUnmount(){
+        DataSource.removeChangeListener(this.handleChange);
+    }
+
+    handleChange(){
+        this.setState({
+            data:selectData(DataSource,this.props)
+        });
+    }
+
+    render(){
+        //被包裹的元素接收container的所有props和新的props，并使用其渲染输出。
+        //高阶组件并不关心数据将会如何或者为什么使用，并且被包裹的元素并不关心数据的源头
+        return <WrappedComponent data={this.state.data} {...this.props }/>;
+    }
+}
+/**
+ * 结果：高阶组件既不会修改输入组件，也不会通过继承来复制行为。相反，通过包裹的形式，高阶组件
+ *       将原先的组件组合在container组件中。高阶组件是纯函数，没有副作用。
+ */
 
 
-export default null;
+/*******************注意事项**********************/
+
+/**
+ * 1.不要改变原始组件，而是使用组合
+ *
+ */
+
+//export default widthSubscription
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
